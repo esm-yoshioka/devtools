0b7e27b219e39b1bbfbf1ff7164bc430679f7808
esm-yoshioka <ken-yohsioka@esm.co.jp>
2022-03-12 01:09:16 +0900

    dockerのインストール資源の変更

--- wsl2/ubuntu_setup.sh
+++ wsl2/ubuntu_setup.sh
@@ -133,8 +133,8 @@ if "$IS_DOCKER" ; then
 
     sudo apt update
     sudo apt -yV upgrade
-    sudo apt install -y ca-certificates curl gnupg lsb-release
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
     sudo apt update
