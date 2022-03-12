043a83cf14c03c7a5f7f5383a950113687a81279
esm-yoshioka <ken-yohsioka@esm.co.jp>
2022-03-12 16:06:59 +0900

    docker-composeをいったん対象外にする

--- wsl2/ubuntu_setup.sh
+++ wsl2/ubuntu_setup.sh
@@ -144,8 +144,8 @@ if "$IS_DOCKER" ; then
 
     sudo usermod -aG docker $DOCKERUSER
 
-    sudo curl -L 'https://github.com/docker/compose/releases/download/'$DOCKERCOMPOSEVER'/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+##    sudo curl -L 'https://github.com/docker/compose/releases/download/'$DOCKERCOMPOSEVER'/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose
+##    sudo chmod +x /usr/local/bin/docker-compose
 fi
 
 #=================================================
