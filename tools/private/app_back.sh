54a7d61cd56d1085d2d82f9bc3bdb912d785c991
esm-yoshioka <ken-yoshioka@esm.co.jp>
2022-04-06 17:22:00 +0900

    オプションでmigrateするか指定

--- tools/private/app_back.sh
+++ tools/private/app_back.sh
@@ -5,11 +5,26 @@ SCRIPT_DIR=$(cd $(dirname $(readlink -f $0 || echo $0));pwd -P)
 cd ${SCRIPT_DIR}
 cd ..
 
+mig_flg=0
+
+while getopts "m" optKey; do
+    case "$opeKey" in
+        m)
+            mig_flg=1
+	    ;;
+	*)
+            ;;
+    esac
+done
+
 # DB起動
 ./localdb start
 
 # migrate
-./localdb migrate
+if [ $mig_flg = 1 ]; then
+    echo '===== DB migrate ====='
+    ./localdb migrate
+fi
 
 #バックエンドを起動
 sh -c "./gradlew bootRun -p server/webapp"
