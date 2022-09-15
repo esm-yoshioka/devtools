67b3a3a926e55d3b3c1a0102ed1eb19bbbada4a3
esm-yoshioka <ken-yoshioka@esm.co.jp>
2022-09-15 23:41:59 +0900

    dockerインストール時の公式GPGキー追加方法を変更

--- wsl2/ubuntu_setup.sh
+++ wsl2/ubuntu_setup.sh
@@ -174,8 +174,10 @@ if "$IS_DOCKER" ; then
     sudo apt update
     sudo apt -yV upgrade
     sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-    curl -SL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     sudo apt update
     sudo apt install -y docker-ce docker-ce-cli containerd.io
