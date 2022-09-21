a618912b955b668b4eb69373a82044c26f739295
esm-yoshioka <ken-yoshioka@esm.co.jp>
2022-09-22 05:05:58 +0900

    セットアップshellの作成

---
+++ tools/fcess_setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+#   F.CESS setup shell
+#     Author :esm-yoshioka
+#
+
+cd ~
+#=================================================
+#   Parameter
+#=================================================
+GIT_HOME="git"
+
+#=================================================
+#   F.CESS
+#=================================================
+echo '===== F.CESS install ====='
+
+if [ ! -d $GIT_HOME ]; then
+    mkdir -p $GIT_HOME
+fi
+
+# git clone
+cd $GIT_HOME
+git clone https://github.com/esminc/fcess-api-spec.git
+git clone https://github.com/esminc/fcess-frontend.git
+git clone https://github.com/esminc/fcess-backend.git
+git clone https://github.com/esminc/fcess-manual.git
+git clone https://github.com/esminc/fcess-manifest.git
+
+cd ~
