0a9cf952d82ff96e5e9b1b8efb2cc0a037304892
esm-yoshioka <ken-yoshioka@esm.co.jp>
2022-10-04 03:25:30 +0900

    まとめて状況確認

---
+++ tools/fcess_statusAll.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+cd ~
+#=================================================
+#   Parameter
+#=================================================
+GIT_HOME="git"
+
+#=================================================
+#   F.CESS
+#=================================================
+cd $GIT_HOME
+
+cd fcess-api-spec
+echo '===== api-spec ====='
+git status
+
+cd ../fcess-frontend
+echo '===== frontend ====='
+git status
+
+cd ../fcess-backend
+echo '===== backend ====='
+git status
+
+cd ../fcess-manual
+echo '===== manual ====='
+git status
+
+cd ../fcess-manifest
+echo '===== manifest ====='
+git status
+
+cd ~/$GIT_HOME
+
