d6f65ccf732a51387f4ca6ae3093c4fa6c89a887
esm-yoshioka <ken-yoshioka@esm.co.jp>
2025-07-04 17:06:36 +0900

    vtermインストール用の資源を追加

--- wsl2/ubuntu_setup.sh
+++ wsl2/ubuntu_setup.sh
@@ -138,7 +138,7 @@ if "$IS_EMACS" ; then
     sudo apt -yV autoremove
     sudo apt autoclean
     sudo snap install emacs --classic
-    sudo apt install -y emacs-mozc-bin cmigemo ripgrep
+    sudo apt install -y emacs-mozc-bin cmigemo ripgrep libtool-bin cmake
 
     [ ! -d .emacs.d ] && mkdir .emacs.d
     [ ! -d .config ] && mkdir .config
